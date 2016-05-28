ffb112fe10a70b058d2c5458c27371a7541fdc58
Sergio Morato <s.mvillar@alumnos.upm.es>
2016-05-28 20:01:42 +0200

    Se crea el ejercicio 2 de la práctica, el Store.

---
+++ PD.EFP/src/main/java/es/upm/miw/epf/store/PeticionLocal.java
@@ -0,0 +1,15 @@
+package es.upm.miw.epf.store;
+
+public class PeticionLocal {
+    public void local1() {
+        // Se realiza la peticion 1
+        System.out.println("[INFO] PeticionLocal:local1");
+    }
+
+    public void local2() {
+        // Se realiza la peticion 1
+        System.out.println("[INFO] PeticionLocal:local2");
+    }
+
+}
+
---
+++ PD.EFP/src/main/java/es/upm/miw/epf/store/Store.java
@@ -0,0 +1,18 @@
+package es.upm.miw.epf.store;
+
+public class Store {
+
+    //TODO a fata de implementar
+
+    // Se almacena un objeto
+    public void write(String key, String value) {
+        //TODO a falta de implementar
+    }
+
+    // Se lee un objeto
+    public String read(String key) {
+        //TODO a falta de implementar
+        return null;
+    }
+
+}
