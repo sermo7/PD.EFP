d3e65dd5f768f33ae12f4f5491b9f9b1b1905173
Sergio Morato <s.mvillar@alumnos.upm.es>
2016-06-19 20:35:31 +0200

    Realizado el test para el ejercicio de peticiones

--- PD.EFP/src/test/java/es/upm/miw/epf/peticiones/PeticionesTest.java
+++ PD.EFP/src/test/java/es/upm/miw/epf/peticiones/PeticionesTest.java
@@ -1,98 +1,70 @@
 package es.upm.miw.epf.peticiones;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.fail;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import es.upm.miw.pd.flyweight.Componente;
-import es.upm.miw.pd.flyweight.FactoriaCaracter;
-import es.upm.miw.pd.flyweight.Parrafo;
-import es.upm.miw.pd.flyweight.Texto;
-
 public class PeticionesTest {
-    private Componente H, o, l, a, pf, txt;
+
+    private PeticionRed peticionRed = new PeticionRed();
+    private PeticionLocal peticionLocal = new PeticionLocal();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Before
-    public void ini() {
-        H = FactoriaCaracter.getFactoria().get('H');
-        o = FactoriaCaracter.getFactoria().get('o');
-        l = FactoriaCaracter.getFactoria().get('l');
-        a = FactoriaCaracter.getFactoria().get('a');
-        pf = new Parrafo();
-        pf.add(H);
-        pf.add(o);
-        pf.add(l);
-        pf.add(a);
-        txt = new Texto();
-        txt.add(pf);
-        txt.add(pf);
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
     }
-
-    @Test
-    public void testCaracterFlyweight() {
-        assertSame(H, FactoriaCaracter.getFactoria().get('H'));
-    }
-
-    @Test
-    public void testCaracterAddCaracter() {
-        H.add(o);
-    }
-
-    @Test
-    public void testDibujarCaracterNormal() {
-        assertEquals("o", o.dibujar(false));
-    }
-
-    @Test
-    public void testDibujarCaracterMayusculas() {
-        assertEquals("O", o.dibujar(true));
-    }
-
-    // ----------------------------------------------
-
-    @Test
-    public void testParrafoNormal() {
-        assertEquals("Hola\n", pf.dibujar(false));
-    }
-
-    @Test
-    public void testParrafoMayusculas() {
-        assertEquals("HOLA\n", pf.dibujar(true));
-    }
-
-    @Test
-    public void testParrafoNoAddParrafo() {
-        try {
-            pf.add(pf);
-            fail("");
-        } catch (UnsupportedOperationException ignored) {
-            ignored.toString();
-        }
-    }
-
-    // ----------------------------------------------
-
-    @Test
-    public void testTextoNormal() {
-        assertEquals("Hola\nHola\n---o---\n",txt.dibujar(false));
+    
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
     }
     
     @Test
-    public void testTextoMayusculas() {
-        assertEquals("HOLA\nHOLA\n---o---\n",txt.dibujar(true));
+    public void out() {
+        System.out.println("hello");
+        assertEquals("hello"+System.getProperty("line.separator"), outContent.toString());
     }
+
+    @Test
+	public void testAccion1(){
+		Accion1 accion1 = new Accion1(peticionRed);
+		accion1.execute();
+		assertEquals("[INFO] PeticionRed:red1"+System.getProperty("line.separator"), outContent.toString());
+	}
     
     @Test
-    public void testTextoNoAddCaracter() {
-        try {
-            txt.add(H);
-            fail();
-        } catch (UnsupportedOperationException ignored) {
-            ignored.toString();
-        }
-    }
+	public void testAccion2(){
+		Accion2 accion2 = new Accion2(peticionRed);
+		accion2.execute();
+		assertEquals("[INFO] PeticionRed:red2"+System.getProperty("line.separator"), outContent.toString());
+	}
+    
+    @Test
+	public void testAccion3(){
+		Accion3 accion3 = new Accion3(peticionRed);
+		accion3.execute();
+		assertEquals("[INFO] PeticionRed:red3"+System.getProperty("line.separator"), outContent.toString());
+	}
+    
+    @Test
+	public void testAccion4(){
+		Accion4 accion4 = new Accion4(peticionLocal);
+		accion4.execute();
+		assertEquals("[INFO] PeticionLocal:local1"+System.getProperty("line.separator"), outContent.toString());
+	}
+    
+    @Test
+	public void testAccion5(){
+		Accion5 accion5 = new Accion5(peticionLocal);
+		accion5.execute();
+		assertEquals("[INFO] PeticionLocal:local2"+System.getProperty("line.separator"), outContent.toString());
+	}
 
 }
